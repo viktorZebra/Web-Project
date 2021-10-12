0d3a9c842489c1afb2d66cb1a7d166978dd532cd
Дмитрий l W.W <shigalkin@mail.ru>
2021-10-12 18:10:25 +0300

    fix exception

---
+++ src/main/kotlin/com/github/viktorzebra/webforum/exception/ForumAlreadyCreatedException.kt
@@ -0,0 +1,5 @@
+package com.github.viktorzebra.webforum.exception
+
+import com.github.viktorzebra.webforum.model.ForumsModel
+
+class ForumAlreadyCreatedException(val forumsModel: ForumsModel) : RuntimeException()
