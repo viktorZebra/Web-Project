5159eb300d6165445262b0ea48507fa623212411
Дмитрий l W.W <shigalkin@mail.ru>
2021-10-13 17:58:48 +0300

    fix: add primary key to forum_users table

--- src/main/kotlin/com/github/viktorzebra/webforum/repository/ForumUsersRepository.kt
+++ src/main/kotlin/com/github/viktorzebra/webforum/repository/ForumUsersRepository.kt
@@ -1,7 +1,13 @@
 package com.github.viktorzebra.webforum.repository
 
 import com.github.viktorzebra.webforum.model.ForumUsersModel
+import com.github.viktorzebra.webforum.model.ForumsModel
+import org.springframework.data.jdbc.repository.query.Query
 import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.query.Param
 
 interface ForumUsersRepository : CrudRepository<ForumUsersModel, Int> {
+
+    @Query("select COUNT(*) from forum_users where user_id = :user_id and forum_id = :forum_id")
+    fun isUserInForumExists(@Param("user_id") user_id: Int, @Param("forum_id") forum_id: Int): Int
 }
